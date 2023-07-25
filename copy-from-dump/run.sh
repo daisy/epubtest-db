0b4dd57ad34a2833d366c9acfeccf3a14af1af7b
Marisa DeMeglio <marisa.demeglio@gmail.com>
2023-07-25 14:02:31 -0700

    update database recreation script

--- copy-from-dump/run.sh
+++ copy-from-dump/run.sh
@@ -9,10 +9,16 @@ read -p "Drop db if exists? (Y/n): " DROP;
 if [ "$DROP" != "n" ];
 then
     psql --set=dbname="$DB_NAME" "host=$DB_HOST port=5432 user=postgres" < dropdb.pgsql
+    wait
 fi
 
 # create the roles
 psql --set=dbname="$DB_NAME" --set=appRolePassword="$DB_PASSWORD" "host=$DB_HOST port=5432 user=postgres" < init-roles.pgsql
+wait
+
+# create the roles
+psql --set=dbname="$DB_NAME" --set=appRolePassword="$DB_PASSWORD" "host=$DB_HOST port=5432 user=postgres" < createdb.pgsql
+wait
 
 if [ -z "$1" ];
 then
@@ -20,6 +26,7 @@ then
 else
     # import the data
     psql --set=dbname="$DB_NAME" "host=$DB_HOST port=5432 user=postgres" < $1
+    wait
 fi
 
 echo "Completed"
